ae6c10a066ab5f9b50364d97108843ec48587bff
ahmedbouzana <ah.bouzana@gmail.com>
2019-01-17 16:15:21 +0100

    AddLikeIcon

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {getMovies} from '../services/movieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = { movies:getMovies()  }
@@ -7,7 +8,15 @@ class Movies extends Component {
 handleDelete = (movie) =>{
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({movies});
-}
+};
+
+handleLike= movie =>{
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = {...movies[index]};
+    movies[index].liked = !movies[index].liked;
+    this.setState({movies});
+};
 
     render() { 
 
@@ -27,6 +36,7 @@ handleDelete = (movie) =>{
                             <th>Stock</th>
                             <th>Rate</th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,6 +46,9 @@ handleDelete = (movie) =>{
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
+                                </td>
                                 <td>
                                     <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
                                 </td>
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,63 +7,72 @@ const movies = [
     genre: { _id: "615", name: "Action" },
     numberInStock: 6,
     dailyRentalRate: 2.5,
-    publishDate: "2018-01-03T19:04:28.809Z"
+    publishDate: "2018-01-03T19:04:28.809Z",
+    liked: true
   },
   {
     _id: "2",
     title: "Die Hard",
     genre: { _id: "615", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2.5,
+    liked: false
   },
   {
     _id: "3",
     title: "Get Out",
     genre: { _id: "619", name: "Thriller" },
     numberInStock: 8,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: true
   },
   {
     _id: "4",
     title: "Trip to Italy",
     genre: { _id: "617", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: false
   },
   {
     _id: "5",
     title: "Airplane",
     genre: { _id: "617", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: true
   },
   {
     _id: "6",
     title: "Wedding Crashers",
     genre: { _id: "617", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: false
   },
   {
     _id: "7",
     title: "Gone Girl",
     genre: { _id: "619", name: "Thriller" },
     numberInStock: 7,
-    dailyRentalRate: 4.5
+    dailyRentalRate: 4.5,
+    liked: true
   },
   {
     _id: "8",
     title: "The Sixth Sense",
     genre: { _id: "619", name: "Thriller" },
     numberInStock: 4,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: false
   },
   {
     _id: "9",
     title: "The Avengers",
     genre: { _id: "615", name: "Action" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3.5,
+    liked: true
   }
 ];
 
