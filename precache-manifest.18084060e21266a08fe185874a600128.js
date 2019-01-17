e883217d0289c4b26e89401a6f2945494ad9e5a0
ahmedbouzana <ah.bouzana@gmail.com>
2019-01-17 14:50:50 +0100

    Updates

---
+++ precache-manifest.18084060e21266a08fe185874a600128.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactJS-Movies-Table/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "bf08c44e2db79535fd1e",
+    "url": "/ReactJS-Movies-Table/static/css/main.f7da577b.chunk.css"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactJS-Movies-Table/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2f5f5e9258d24efb3542",
+    "url": "/ReactJS-Movies-Table/static/js/1.2f5f5e92.chunk.js"
+  },
+  {
+    "revision": "5e55a3b083594ebe5a7c",
+    "url": "/ReactJS-Movies-Table/static/js/runtime~main.5e55a3b0.js"
+  },
+  {
+    "revision": "bf08c44e2db79535fd1e",
+    "url": "/ReactJS-Movies-Table/static/js/main.bf08c44e.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactJS-Movies-Table/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactJS-Movies-Table/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactJS-Movies-Table/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2f5f5e9258d24efb3542",
+    "url": "/ReactJS-Movies-Table/static/css/1.6a3e4ad2.chunk.css"
+  },
+  {
+    "revision": "ca4be71442eb7de46cfd57c494280f6a",
+    "url": "/ReactJS-Movies-Table/index.html"
+  }
+];
