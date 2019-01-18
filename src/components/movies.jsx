5bb41b1898779b5c204d70a465c3f815c8562b65
ahmedbouzana <ah.bouzana@gmail.com>
2019-01-18 18:53:09 +0100

    AddPagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,65 +1,85 @@
-import React, { Component } from 'react';
-import {getMovies} from '../services/movieService';
-import Like from './common/like';
+import React, { Component } from "react";
+import { getMovies } from "../services/movieService";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
-    state = { movies:getMovies()  }
+  state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
 
-handleDelete = (movie) =>{
+  handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({movies});
-};
+    this.setState({ movies });
+  };
 
-handleLike= movie =>{
+  handleLike = movie => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
-    movies[index] = {...movies[index]};
+    movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
-    this.setState({movies});
-};
+    this.setState({ movies });
+  };
 
-    render() { 
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
 
-        const{length : count} = this.state.movies;
+  render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
-        if(count === 0)
-            return <p>There are no movies in the database.</p>;
+    if (count === 0) return <p>There are no movies in the database.</p>;
 
-        return (
-            <React.Fragment>
-                <p>Showing {count} movies in the database.</p>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
-                                </td>
-                                <td>
-                                    <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
-                                </td>
-                            </tr> 
-                        ))}
-                        
-                    </tbody>
-                </table>
-            </React.Fragment>        
-        );
-    }
+    const movies = paginate(allMovies, currentPage, pageSize);
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Movies;
